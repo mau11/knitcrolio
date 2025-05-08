10fd459edb8ef4cc2860f8223d00bd6e918ee118
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-05-08 03:24:13 -0400

    Update RadioInput component styling

--- src/components/form/RadioInput.tsx
+++ src/components/form/RadioInput.tsx
@@ -19,24 +19,32 @@ export const RadioInput = ({
   register,
 }: RadioInputProps) => {
   return (
-    <fieldset className="mt-8 block border border-gray-400 rounded-sm p-2 relative">
-      <legend className="absolute -top-3 left-1/2 -translate-x-1/2 px-1 bg-background text-gray-700 block text-sm font-medium w-max">
-        {label}
-      </legend>
-      <div>
-        {options.map((option) => (
-          <label key={option.value} className="flex items-center space-x-2">
-            <input
-              type="radio"
-              value={option.value}
-              {...register(fieldName)}
-              className="border-gray-400"
-            />
-            <span>{option.label}</span>
-          </label>
-        ))}
-        {error && <p className="text-red-500 text-sm">{error}</p>}
-      </div>
-    </fieldset>
+    <>
+      <fieldset
+        className={`mt-8 block border border-gray-400 rounded-sm p-2 relative ${
+          error ? "border-red-500" : "border-gray-400"
+        }`}
+      >
+        <legend className="absolute -top-3 left-1/2 -translate-x-1/2 px-1 bg-background text-gray-700 block text-sm font-medium w-max">
+          {label}
+        </legend>
+        <div className="flex  space-x-2">
+          {options.map((option) => (
+            <label
+              key={option.value}
+              className="flex items-center space-x-1 px-1"
+            >
+              <input
+                type="radio"
+                value={option.value}
+                {...register(fieldName)}
+              />
+              <span>{option.label}</span>
+            </label>
+          ))}
+        </div>
+      </fieldset>
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </>
   );
 };
