c7d3eda61a266a028523c441378a9799fc2e1e29
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-05-15 19:53:03 -0400

    Remove amountUsed from Yarn / Inventory join table

---
+++ prisma/migrations/20250515235045_remove_amount_used_from_yarn/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `amountUsed` on the `YarnOnInventory` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "YarnOnInventory" DROP COLUMN "amountUsed";
