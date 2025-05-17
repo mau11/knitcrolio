287b9a1fb37c16ea32a2c5145f5c02ed3b5124f0
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-05-17 17:22:27 -0400

    Allow user to edit product

--- src/app/api/editProduct/[id]/route.ts
+++ src/app/api/editProduct/[id]/route.ts
@@ -8,23 +8,46 @@ export async function PUT(
   request: Request,
   { params }: { params: { id: number } }
 ) {
-  const { id } = await params;
-  const data: Inventory = await request.json();
-
-  const result = inventorySchema.safeParse(data);
-
-  if (!result.success) {
-    return new Response(JSON.stringify({ error: result.error.flatten() }), {
-      status: 400,
-    });
-  }
-
-  const validData = result.data;
-
   try {
-    await prisma.inventory.update({
-      where: { id: Number(id) },
-      data: validData,
+    const { id } = await params;
+    const data: Inventory = await request.json();
+
+    const result = inventorySchema.safeParse(data);
+
+    if (!result.success) {
+      return new Response(JSON.stringify({ error: result.error.flatten() }), {
+        status: 400,
+      });
+    }
+
+    const { yarnUsed, ...validData } = result.data;
+    const inventoryId = Number(id);
+
+    await prisma.$transaction(async (tx) => {
+      // Update the inventory data
+      await tx.inventory.update({
+        where: { id: inventoryId },
+        data: validData,
+      });
+
+      // Disconnect existing yarn associations for this inventory item
+      await tx.yarnOnInventory.deleteMany({
+        where: {
+          inventoryId: inventoryId,
+        },
+      });
+
+      // Connect the newly selected yarn list
+      await tx.inventory.update({
+        where: { id: Number(id) },
+        data: {
+          yarnUsed: {
+            create: yarnUsed.map((id) => ({
+              yarn: { connect: { id: parseInt(id, 10) } },
+            })),
+          },
+        },
+      });
     });
 
     return NextResponse.json(
---
+++ src/app/inventory/edit/page.tsx
@@ -0,0 +1,12 @@
+import InventoryForm from "@components/InventoryForm";
+
+const EditYarn = () => {
+  return (
+    <div className="py-4">
+      <h1 className="text-2xl mb-4">Edit Product</h1>
+      <InventoryForm />
+    </div>
+  );
+};
+
+export default EditYarn;
